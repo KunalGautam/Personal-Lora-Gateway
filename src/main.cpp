5f59e76709a5f13c0c619af5278ac95fb19d9139
Kunal Gautam <kunal@ikunal.in>
2021-05-03 22:42:33 +0530

    Basic HTML Page Done.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
 #include <Arduino.h>
+#include <DNSServer.h>
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+#include "ESPAsyncWebServer.h"
 
-void setup() {
-  // put your setup code here, to run once:
+DNSServer dnsServer;
+AsyncWebServer server(80); 
+
+void setup(){
+   Serial.begin(115200);
+ 
+  if(!SPIFFS.begin()){
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+  }
+  //your other setup stuff...
+  WiFi.softAP("esp-captive");
+  dnsServer.start(53, "*", WiFi.softAPIP());
+  //more handlers...
+  server.begin();
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+  server.on("/a.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/a.css", "text/css");
+  });
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  dnsServer.processNextRequest();
 }
